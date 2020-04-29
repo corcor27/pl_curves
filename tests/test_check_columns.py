13eddcee124bf1b41f7c01313a9f5d22b0287753
corcor27 <cory13579@googlemail.com>
2020-04-29 12:15:52 +0100

    Update test_check_columns.py

--- tests/test_check_columns.py
+++ tests/test_check_columns.py
@@ -4,24 +4,8 @@ import pandas
 
 
 def test_check_columns_correct():
-    '''
-    Checks all columns in the data frame sum to 1
-    Parameters
-    ----------
-    dataframe
-        The data frame to check
-    Returns
-    -------
-    Bool
-        False if a column doesn't sum to 1, True if they all do
-    '''
-    for col in dataframe.columns:
-        total = sum(dataframe.loc[:, col])
-        # print(col, "total=", total)
-        if total < 0.9999 or total > 1.0001:
-            # print("Error column ", col, "doesn't sum to 1.0")
-            return False
-    return True
+    df = pandas.DataFrame([0.2 0.8])
+    assert check_columns(df) is True
 
 
 def test_check_columns_incorrect():
